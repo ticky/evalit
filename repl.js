acaabf7a944a04f66b1711bba64da16ea07ca4e0
Jessica Stokes <hello@jessicastokes.net>
2015-06-03 21:03:18 +1000

    First decent version of the EvalIt JavaScript REPL

---
+++ repl.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function(sandboxedEval) {
+  var output = document.getElementById('output');
+
+  function createRow(text, type, error) {
+    appendRow(document.createTextNode(text), type, error);
+  }
+
+  function appendRow(content, type, error) {
+    var row = document.createElement('li');
+    row.classList.add(type);
+    if (error) {
+      row.classList.add('error');
+    }
+    if (content instanceof Array) {
+      for (var element in content) {
+        row.appendChild(content[element]);
+      }
+    } else {
+      row.appendChild(content);
+    }
+    output.appendChild(row);
+    output.scrollTop = output.scrollHeight;
+  }
+
+  function runAndShowResult(value) {
+    createRow(value, 'input', false);
+
+    var result, resultString, error = false;
+
+    try {
+      result = sandboxedEval(value);
+      resultString = typeof result !== 'undefined'
+        ? JSON.stringify(result, null, ' ')
+          .replace(/\n/g, '')
+          .replace(/\{\s*/g, '{')
+          .replace(/\s*\}/g, '}')
+        : result;
+    } catch (e) {
+      resultString = e;
+      error = true;
+    }
+
+    createRow(resultString, 'output', error);
+  }
+
+  document.getElementById('input').addEventListener('submit', function(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    var inputValue = document.getElementById('inputValue');
+    var value = inputValue.value;
+    inputValue.value = '';
+
+    runAndShowResult(value);
+  });
+}
+(function(code) {return eval(code);}));
